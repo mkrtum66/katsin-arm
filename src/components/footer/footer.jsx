2edcb94497ec51bd2c697ad3df999f9940134376
iMac <imac@iMacs-iMac.local>
2023-09-11 15:40:44 -0700

    fixes

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import './footer.scss';
 import location from '../../assets/icons/location.png';
 import phone from '../../assets/icons/call.png';
+import calendar from '../../assets/icons/calendar.png';
+import clock from '../../assets/icons/clock.png';
+import mail from '../../assets/icons/email.png';
+import logo from '../../assets/logo-big.png';
 import { Form } from 'react-bootstrap';
 import Button from '../button';
 import { NavLink } from 'react-router-dom';
@@ -23,7 +27,7 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="container">
-        <div className="row">
+        <div className="row justify-content-between align-items-end">
           <div className="col-md-3 mb-5 mb-md-0">
             <div className="col1">
               <a
@@ -32,34 +36,37 @@ const Footer = () => {
                 rel="noreferrer"
                 className="location-link"
               >
-                <p>Address:</p>
                 <div>
                   <img src={location} alt="img" />
-                  <span>
-                    515 W Broadway Ave, Unit 111, <br />
-                    Glendale, CA <br />
-                    91204
-                  </span>
+                  <span>515 W Broadway Ave, Unit 111, Glendale, CA, 91204</span>
                 </div>
               </a>
               <a href="tel:+18183311000" className="phone-link">
                 <img src={phone} alt="img" />
                 <span>+1 (818)-331-1000</span>
               </a>
-            </div>
-          </div>
-          <div className="col-md-3 mb-5 mb-md-0">
-            <div className="hours-wrapper">
-              <p className="title">Hours:</p>
-              <div className="hours">
-                <p>Monday - Sunday</p>
-                <p style={{ height: '24px' }}></p>
-                <p>12:30PM - 0:00AM</p>
+              <div className="hours mb-3">
+                <div className="mb-3">
+                  <img src={calendar} alt="img" />
+                  <p>Monday - Sunday</p>
+                </div>
+                <div>
+                  <img src={clock} alt="img" />
+                  <p>12:00PM - 0:00AM</p>
+                </div>
               </div>
-              <p className="closed m-0">Closed for most major holidays</p>
+              <div className="email">
+                <a href="mailto:info@katsinusa.com">
+                  <img src={mail} alt="img" />
+                  <p>info@katsinusa.com</p>
+                </a>
+              </div>
             </div>
           </div>
           <div className="col-md-3 mb-5 mb-md-0">
+            <div className="logo-wrapper mb-4">
+              <img src={logo} alt="logo" className="img-fluid" />
+            </div>
             <p className="title">Newslatters:</p>
             {!isSubmitted ? (
               <Form className="newsLatterForm" onSubmit={event => handleSubmit(event)}>
@@ -78,24 +85,62 @@ const Footer = () => {
             )}
           </div>
           <div className="col-md-3">
-            <ul className="footer-links">
-              <li>
-                <NavLink to={'/jobs'}>Jobs</NavLink>
-              </li>
-              <li>
-                <NavLink to={'/privacy-policy'}>Privacy Policy</NavLink>
-              </li>
-              <li>
-                <NavLink to={'/terms-of-services'}>Terms of Services</NavLink>
-              </li>
-              <li>
-                <NavLink to={'/accessibility'}>Accessibility</NavLink>
-              </li>
-            </ul>
+            <div className="col1">
+              <a
+                href="https://www.google.com/maps/place/Katsin+Restaurant/@40.179765,44.5099792,19.91z/data=!4m15!1m8!3m7!1s0x406abcfbf7f7bfa7:0x621287d0a9083be3!2s4,+6+Amiryan+St,+Yerevan+0001,+Armenia!3b1!8m2!3d40.1795514!4d44.5102382!16s%2Fg%2F11r63j0y84!3m5!1s0x406abde086cd6025:0xca014a370e8a581!8m2!3d40.1798355!4d44.5107282!16s%2Fg%2F11rmzkfx17?entry=ttu"
+                target="_blank"
+                rel="noreferrer"
+                className="location-link"
+              >
+                <div>
+                  <img src={location} alt="img" />
+                  <span>4/6 Amiryan Str., Yerevan, Armenia</span>
+                </div>
+              </a>
+              <a href="tel:+37477881001" className="phone-link">
+                <img src={phone} alt="img" />
+                <span>+374 77 881001</span>
+              </a>
+              <div className="hours">
+                <div className="mb-3">
+                  <img src={calendar} alt="img" />
+                  <p>Monday - Sunday</p>
+                </div>
+                <div className="mb-3">
+                  <img src={clock} alt="img" />
+                  <p>11:00PM - 1:00AM</p>
+                </div>
+              </div>
+              <div className="email">
+                <a href="mailto:katsin@tiziano.am">
+                  <img src={mail} alt="img" />
+                  <p>katsin@tiziano.am</p>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
+        <div className="row mt-5">
+          <ul className="footer-links">
+            <li>
+              <NavLink to={'/jobs'}>Jobs</NavLink>
+            </li>
+            <span>|</span>
+            <li>
+              <NavLink to={'/privacy-policy'}>Privacy Policy</NavLink>
+            </li>
+            <span>|</span>
+            <li>
+              <NavLink to={'/terms-of-services'}>Terms of Services</NavLink>
+            </li>
+            <span>|</span>
+            <li>
+              <NavLink to={'/accessibility'}>Accessibility</NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
-      <p className="poweredBy mt-5 mb-0">
+      <p className="poweredBy mb-0">
         Powered by <a href="https://www.linkedin.com/in/mkrtum-abrahamyan/">M. A.</a>
       </p>
     </div>
